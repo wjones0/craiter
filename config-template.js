c845770e216a12cfd16740b6a92bc9fad24cce29
William Jones <wjones0@users.noreply.github.com>
2015-08-01 12:06:16 -0400

    finish up base functionality.  reads db, gets rss, DM in twitter to the user.

--- config-template.js
+++ config-template.js
@@ -1,8 +1,10 @@
 var config = {};
 
 config.twitter = {};
-config.twitter.user_name = process.env.TWITTER_USER || 'username';
-config.twitter.password=  process.env.TWITTER_PASSWORD || 'password';
+config.twitter.consumer_key = 'key';
+config.twitter.consumer_secret = 'sec';
+config.twitter.access_token_key = 'key';
+config.twitter.access_token_secret = 'sec';
 
 config.mongodb = {};
 config.mongodb.connection =  'mongodb://mongo';
--- index.js
+++ index.js
@@ -1,10 +1,63 @@
 var config = require('./config');
 var mongodb = require('mongodb');
+var feed = require("feed-read");
+var Twitter = require('twitter');
 
 
 var MongoClient = mongodb.MongoClient;
 
 var url = config.mongodb.connection;
+
+
+function processUser(user, saveCallBack) {
+
+  for (var i = 0; i < user.search_strings.length; i++) {
+    // console.log(user.search_strings[i].search);
+    var jsonString = JSON.stringify(user.posts);
+    
+    var searchparam = user.search_strings[i];
+    
+    feed(searchparam.search, function (err, articles) {
+      if (err) throw err;
+
+      for (var j = 0; j < articles.length; j++) {
+        console.log(articles[j].link);
+        if (jsonString.indexOf(articles[j].link) > -1) {
+          console.log("your key or value exists!");
+        } else {
+          user.posts.push({"url":articles[j].link});
+          tweet(user.username, searchparam.name, articles[j]);
+        }
+      }
+        console.log(user.posts);
+        saveCallBack(user);
+        // save user
+    });
+  }
+
+}
+
+function tweet(username, searchname, post)
+{
+ 
+    var client = new Twitter({
+      consumer_key: config.twitter.consumer_key,
+      consumer_secret: config.twitter.consumer_secret,
+      access_token_key: config.twitter.access_token_key,
+      access_token_secret: config.twitter.access_token_secret
+    });
+       
+    var dmText = searchname + ": " + post.link;
+       
+    client.post('direct_messages/new', {screen_name: username, text: dmText},  function(error, tweet, response){
+      if(error) throw error;
+      // console.log(tweet);  // Tweet body. 
+      // console.log(response);  // Raw response object. 
+    });
+
+}
+
+
 
 
 MongoClient.connect(url, function (err, db) {
@@ -17,18 +70,34 @@ MongoClient.connect(url, function (err, db) {
     var collection = db.collection('creets');
 
     var cursor = collection.find();
+    cursor.count(function(err,count) {
+      var savesPending = count;
 
-    cursor.each(function (err, doc) {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log('Fetched:', doc);
+      if(count == 0){
+        db.close();
+        return;
+      }
+
+      var saveFinished = function(){
+        savesPending--;
+        if(savesPending == 0){
+          db.close();
+        }
       }
       
-      //Close connection
-      db.close();
-    });
+      var saveUser = function (doc) {
+        collection.save(doc, {safe:true}, saveFinished);
+      }
 
-
+      cursor.each(function (err, doc) {
+        if (doc != null) {
+          processUser(doc, saveUser);
+        }
+      });
+    })
   }
 });
+
+
+
+
