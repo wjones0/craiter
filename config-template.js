e9d2458106798f0afb1a0120edc9e3c3d943b3fa
William Jones <wjones0@users.noreply.github.com>
2015-07-31 22:57:14 -0400

    connection to mongodb established

--- config-template.js
+++ config-template.js
@@ -1,11 +1,10 @@
 var config = {};
 
 config.twitter = {};
-
-
 config.twitter.user_name = process.env.TWITTER_USER || 'username';
 config.twitter.password=  process.env.TWITTER_PASSWORD || 'password';
 
+config.mongodb = {};
 config.mongodb.connection =  'mongodb://mongo';
 
 
--- index.js
+++ index.js
@@ -7,3 +7,28 @@ var MongoClient = mongodb.MongoClient;
 var url = config.mongodb.connection;
 
 
+MongoClient.connect(url, function (err, db) {
+  if (err) {
+    console.log('Unable to connect to the mongoDB server. Error:', err);
+  } else {
+    //HURRAY!! We are connected. :)
+    console.log('Connection established to mongodb');
+
+    var collection = db.collection('creets');
+
+    var cursor = collection.find();
+
+    cursor.each(function (err, doc) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('Fetched:', doc);
+      }
+      
+      //Close connection
+      db.close();
+    });
+
+
+  }
+});
